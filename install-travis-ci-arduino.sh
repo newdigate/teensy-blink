9a74e494f2216138de97b3f8c3c332abf6143b8b
Nic Newdigate <nicnewdigate@gmail.com>
2019-02-27 11:32:10 +0000

    Update install-travis-ci-arduino.sh

--- install-travis-ci-arduino.sh
+++ install-travis-ci-arduino.sh
@@ -39,9 +39,9 @@ echo -e "\n#####################################################################
 echo -e "${YELLOW}INSTALLING arm-none-eabi"
 echo "########################################################################";
 
-
-if [ ! -f $HOME/arduino_ide/hardware/tools/arm ]; then
+echo -e $PWD
 origdir=$(PWD)
+if [ ! -f $HOME/arduino_ide/hardware/tools/arm ]; then
 mkdir -p $HOME/arduino_ide/hardware/tools/arm
 cd $HOME/arduino_ide/hardware/tools/arm
 wget --quiet -O gccarmnoneeabi542016q220160622linux.tar.bz2 https://developer.arm.com/-/media/Files/downloads/gnu-rm/5_4-2016q2/gccarmnoneeabi542016q220160622linuxtar.bz2?revision=8f445a99-c1ae-4ed8-9eb8-f41929a671c4?product=GNU-RM%20Downloads,32-bit,,Linux,5-2016-q2-update
