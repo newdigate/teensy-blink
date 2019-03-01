9ebe8156df0b4779c1113ec8827e208b54a4a6af
Nic Newdigate <nicnewdigate@gmail.com>
2019-03-01 20:32:20 +0000

    Update install-travis-ci-arduino.sh

--- install-travis-ci-arduino.sh
+++ install-travis-ci-arduino.sh
@@ -10,7 +10,7 @@ fi
 # this will be eval'd in the functions below because arrays can't be exported
 # Uno is ATmega328, Zero is SAMD21G18, ESP8266, Leonardo is ATmega32u4, M4 is SAMD51, Mega is ATmega2560, ESP32
 # export MAIN_PLATFORMS='declare -A main_platforms=( [teensy]="teensy:avr:teensy36" [uno]="arduino:avr:uno" [due]="arduino:sam:arduino_due_x" [zero]="arduino:samd:arduino_zero_native" [esp8266]="esp8266:esp8266:huzzah:eesz=4M3M,xtal=80" [leonardo]="arduino:avr:leonardo" [m4]="adafruit:samd:adafruit_metro_m4" [mega2560]="arduino:avr:mega:cpu=atmega2560" [esp32]="esp32:esp32:featheresp32:FlashFreq=80" )'
-export MAIN_PLATFORMS='declare -A main_platforms=( [teensy]="teensyduino:avr:teensy36" )'
+export MAIN_PLATFORMS='declare -A main_platforms=( [teensy]="teensyduino:avr:teensy36:usb=serial,speed=180,opt=o2std,keys=en-us" )'
 
 # define colors
 GRAY='\033[1;30m'; RED='\033[0;31m'; LRED='\033[1;31m'; GREEN='\033[0;32m'; LGREEN='\033[1;32m'; ORANGE='\033[0;33m'; YELLOW='\033[1;33m'; BLUE='\033[0;34m'; LBLUE='\033[1;34m'; PURPLE='\033[0;35m'; LPURPLE='\033[1;35m'; CYAN='\033[0;36m'; LCYAN='\033[1;36m'; LGRAY='\033[0;37m'; WHITE='\033[1;37m';
